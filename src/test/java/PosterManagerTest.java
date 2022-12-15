ab13a1c57e32564ba6b1762c821a58e3e4beeea9
Elena Isaeva <isayeva.yelena.94@mail.ru>
2022-12-15 10:22:34 +0300

    create gitignore

--- src/test/java/PosterManagerTest.java
+++ src/test/java/PosterManagerTest.java
@@ -57,4 +57,71 @@ public class PosterManagerTest {
 
         Assertions.assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void testFindLast11posters() {
+        PosterManager manager = new PosterManager();
+
+        manager.addPoster("Бладшот");
+        manager.addPoster("Вперед");
+        manager.addPoster("Отель Белград");
+        manager.addPoster("Джентельмены");
+        manager.addPoster("Человек-невидимка");
+        manager.addPoster("Тролли. Мировой тур");
+        manager.addPoster("Номер один");
+        manager.addPoster("Отель");
+        manager.addPoster("Джин");
+        manager.addPoster("Человек в железной маске");
+        manager.addPoster("Троя");
+
+
+        String[] expected = {"Троя", "Человек в железной маске", "Джин", "Отель", "Номер один", "Тролли. Мировой тур", "Человек-невидимка", "Джентельмены", "Отель Белград","Вперед"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindLast10posters() {
+        PosterManager manager = new PosterManager();
+
+        manager.addPoster("Бладшот");
+        manager.addPoster("Вперед");
+        manager.addPoster("Отель Белград");
+        manager.addPoster("Джентельмены");
+        manager.addPoster("Человек-невидимка");
+        manager.addPoster("Тролли. Мировой тур");
+        manager.addPoster("Номер один");
+        manager.addPoster("Отель");
+        manager.addPoster("Джин");
+        manager.addPoster("Человек в железной маске");
+
+
+        String[] expected = {"Человек в железной маске", "Джин", "Отель", "Номер один", "Тролли. Мировой тур", "Человек-невидимка", "Джентельмены", "Отель Белград", "Вперед", "Бладшот"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindLast9posters() {
+        PosterManager manager = new PosterManager();
+
+        manager.addPoster("Бладшот");
+        manager.addPoster("Вперед");
+        manager.addPoster("Отель Белград");
+        manager.addPoster("Джентельмены");
+        manager.addPoster("Человек-невидимка");
+        manager.addPoster("Тролли. Мировой тур");
+        manager.addPoster("Номер один");
+        manager.addPoster("Отель");
+        manager.addPoster("Джин");
+
+
+        String[] expected = { "Джин", "Отель", "Номер один", "Тролли. Мировой тур", "Человек-невидимка", "Джентельмены", "Отель Белград", "Вперед", "Бладшот" };
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
 }
